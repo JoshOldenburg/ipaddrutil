ef0401ccc242d6e2c1bf3cf97f5ec8fb063e6760
Josh Oldenburg <joshthegeek@me.com>
2014-07-27 19:10:23 -0400

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,10 @@
+CC=gcc # I'd use clang, but gcc links to clang on OS X anyways
+CFLAGS=-Wall # -Weverything is clang-specific
+
+all: getifaddrs
+
+getifaddrs:
+	$(CC) $(CFLAGS) getifaddrs.c -o getifaddrs
+
+clean:
+	rm getifaddrs
---
+++ getifaddrs.c
@@ -0,0 +1,42 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, const char *argv[]) {
+	const char *interface = "en1";
+	if (argc > 1) interface = argv[1];
+	if (strcmp(interface, "-a") == 0) interface = "";
+
+	struct ifaddrs *ifaddr;
+
+	if (getifaddrs(&ifaddr) == -1) {
+		perror("getifaddrs");
+		freeifaddrs(ifaddr);
+		return EXIT_FAILURE;
+	}
+
+
+	for (struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+		if (!ifa->ifa_addr) continue;
+
+		if (strncmp(ifa->ifa_name, interface, strlen(interface)) != 0 || ifa->ifa_addr->sa_family != AF_INET) continue;
+
+		char host[NI_MAXHOST];
+		int s = 0;
+		if ((s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST)) != 0) {
+			printf("getnameinfo() failed: %s\n", gai_strerror(s));
+			freeifaddrs(ifaddr);
+			return EXIT_FAILURE;
+		}
+
+		printf("%s:%s\n", ifa->ifa_name, host);
+	}
+
+	freeifaddrs(ifaddr);
+	return EXIT_SUCCESS;
+}
